d3ad317b59747e7c842ca7f03f948c52a177f884
yhcho0405 <yhcho0405@kakao.com>
2021-06-18 14:04:27 +0900

    feat(stack): add stack implementation for push_swap functionality

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: youncho <youncho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 21:44:14 by youncho           #+#    #+#             */
-/*   Updated: 2021/06/15 21:46:10 by youncho          ###   ########.fr       */
+/*   Updated: 2021/06/18 13:01:13 by youncho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 
 # include "../libft/libft.h"
 
+typedef struct s_node
+{
+	int				val;
+	struct s_node	*prev;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*head;
+	size_t	size;
+}	t_stack;
+
 
 #endif
+
